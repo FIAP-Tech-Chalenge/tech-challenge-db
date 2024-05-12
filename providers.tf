a8999d853e17c12f75bc1e718194bc2d5f176c36
Matheus Telles <business.mwtelles@gmail.com>
2024-05-12 11:59:23 -0300

    corrigindo providers

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 
   assume_role {
     role_arn     = "arn:aws:iam::135488045194:role/voclabs/user3240016=business.mwtelles@gmail.com"
